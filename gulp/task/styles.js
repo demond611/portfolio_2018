c0aef331061566146ceee9c123f73144cbfe43c4
Robert <demond611@gmail.com>
2018-02-26 01:40:36 -0600

    working on index.html

---
+++ gulp/task/styles.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
+
+
+gulp.task('styles', function(){
+	return gulp.src('./app/assets/styles/main.css')
+	.pipe( postcss([cssImport, cssvars, nested, autoprefixer]) )
+	.on('error', function(errorInfo){
+		console.log("\nError: "+errorInfo.toString()+"\n");
+		this.emit('end'); // ENDS TASK CALL SUCCESSFULLY ALWAYS
+	})
+	.pipe( gulp.dest('./app/temp/styles') );
+});
---
+++ gulp/task/watch.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
+
+
+// WATCH TASK FOR STYLES,HTML,JS UPDATES
+gulp.task('watch', function(){
+
+	browserSync.init({
+		notify: false,
+		server:{
+			baseDir: "app"
+		}
+	});
+
+	watch('./app/index.html', function(){
+		browserSync.reload();
+	});
+
+	watch('./app/assets/styles/**/*.css', function(){
+		gulp.start('cssInject');
+	});
+
+});
+
+
+// CSS INJECT FOR STYLES UPDATES
+gulp.task('cssInject', ['styles'], function(){
+	return gulp.src('./app/temp/styles/main.css')
+	.pipe(browserSync.stream());
+});
